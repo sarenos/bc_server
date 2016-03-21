c2a690eed01f4475f030ab73fc4a7285a8055204
eugene_oleynyak <eugene_oleynyak@epam.com>
2016-03-21 15:09:54 +0200

    find

--- protected/handlers/main/find/model.inc.php
+++ protected/handlers/main/find/model.inc.php
@@ -18,30 +18,26 @@ class MainFindModel extends MainModel
 
     public function action_find()
     {
-        return array_merge(
-            $this->_load_by_find()
-        );
+    	$this->Result = array('data' => $this->_load_by_find());
     }
 
     private function _load_by_find()
     {
-        $user1 = (string)@$_GET["find_nick"];		
+        $user1 = (string)@$_GET["find_nick"];	
+            
         $this->_DBHandler->exec_query(
             "SELECT ". User::SQL_USER_DATA
             . ", loc.latitude AS lat, loc.longitude AS lng,"
             . $this->_User->SQL_FILTER_ONLINE
             . " FROM `bc_locations` AS loc, `bc_users_info`"
-            . "WHERE `bc_users_info`.user_id = loc.user_id and bc_users_info.nick like ".$user1 
+            . "WHERE `bc_users_info`.user_id = loc.user_id and bc_users_info.nick like '".$user1."%'" 
         );
         $res_rec = array();
         foreach ($this->_DBHandler->get_all_data() as $record)
         {
                 $record['isOnline'] = $record['isOnline'] ? true : false;
-            $res_rec[] = array_merge(
-                $record,
-                array(
-                    'friend'    => 0
-                ));        }
+            $res_rec[] = $record;    
+        }
         return $res_rec;
     }
 
