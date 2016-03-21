980f24159d34f8405a6eb85f2589eb2633136a9a
eugene_oleynyak <eugene_oleynyak@epam.com>
2016-03-21 14:56:54 +0200

    find

--- protected/handlers/main/find/model.inc.php
+++ protected/handlers/main/find/model.inc.php
@@ -31,7 +31,7 @@ class MainFindModel extends MainModel
             . ", loc.latitude AS lat, loc.longitude AS lng,"
             . $this->_User->SQL_FILTER_ONLINE
             . " FROM `bc_locations` AS loc, `bc_users_info`"
-            . "WHERE `bc_users_info`.user_id = loc.user_id and bc_users_info.nick like '".$user1."%'" 
+            . "WHERE `bc_users_info`.user_id = loc.user_id and bc_users_info.nick like ".$user1 
         );
         $res_rec = array();
         foreach ($this->_DBHandler->get_all_data() as $record)
