62a8e6f23851915f298f604f388b2db586733624
eugene_oleynyak <eugene_oleynyak@epam.com>
2016-03-21 13:08:27 +0200

    find

---
+++ protected/handlers/main/find/model.inc.php
@@ -0,0 +1,59 @@
+<?php
+
+require_once LAYERS_DIR . '/User/user.php';
+require_once LAYERS_DIR . '/Location/location.php';
+
+class MainFindModel extends MainModel
+{
+    private $_User, $_Location;
+    private $_DBHandler;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->_User = new User();
+        $this->_Location = new Location();
+        $this->_DBHandler = produce_db();
+    }
+
+    public function find()
+    {
+        return array_merge(
+            $this->_load_by_find()
+        );
+    }
+
+    private function _load_by_find()
+    {
+        $user1 = (string)@$_GET["find_nick"];
+        $this->_DBHandler->exec_query(
+            "SELECT ". User::SQL_USER_DATA
+            . ", loc.latitude AS lat, loc.longitude AS lng,"
+            . "find.status, " . $this->_User->SQL_FILTER_ONLINE
+            . " FROM `bc_locations` AS loc, `bc_users_info`"
+            . " JOIN (SELECT * FROM `bc_users_info` WHERE nick LIKE '".$user1."') AS find"
+            . " ON `bc_users_info`.user_id = find.user2 "
+            . "WHERE `bc_users_info`.user_id = loc.user_id"
+        );
+        $res_rec = array();
+        foreach ($this->_DBHandler->get_all_data() as $record)
+        {
+                $record['isOnline'] = $record['isOnline'] ? true : false;
+            $res_rec[] = array_merge(
+                $record,
+                array(
+                    'friend'    => 0
+                ));        }
+        return $res_rec;
+    }
+
+    public function action_default()
+    {
+    }
+    
+    public function run()
+    {
+        parent::run();
+        $this->determine_action();
+    }
+}
---
+++ protected/handlers/main/find/view.inc.php
@@ -0,0 +1,9 @@
+<?php
+
+class MainFindView extends ViewAjax
+{
+	public function __construct()
+	{
+		parent::__construct();
+	}
+}
